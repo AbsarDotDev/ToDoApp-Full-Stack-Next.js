cd770d0965d76167db3633781a30e58bbfa80563
Hafiz Absar Ali <absarali701@gmail.com>
2023-05-30 01:28:20 +0500

    Fonts Improved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='bg-gradient-to-tr from-primary to-secondary h-screen
-    flex justify-center items-center'>
-      <div className='px-6 py-8 rounded-xl bg-gradient-to-br from-[#D9D9D9]/50 to-[#D9D9D9]/60  backdrop-blur-xl w-full max-w-md'>
+    flex flex-col justify-center items-center '>
+      <div className='px-6 py-8  rounded-xl bg-gradient-to-br from-[#D9D9D9]/50 to-[#D9D9D9]/60  backdrop-blur-xl w-full max-w-md'>
         {/* Todo List */}
         {/* @ts-ignore */}
         <TodoList />
@@ -16,6 +16,7 @@ export default function Home() {
         {/* Bar */}
         <div className='w-1/2 h-1.5 bg-black/80 rounded mx-auto mt-6 '></div>
       </div>
+      <div className='mt-[100px]'> <h1 className='text-4xl text-white'>Made By Absar Ali</h1></div>
     </main>
   )
 }
